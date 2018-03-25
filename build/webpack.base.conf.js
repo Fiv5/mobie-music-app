543f7e3c93531387caef1872f83dc76f219879e5
Fiv5 <Fiv5@MacdeMacBook-Pro.local>
2018-03-25 16:12:38 +0800

    add changes

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -35,8 +35,9 @@ const xxx = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      vue$: 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      src: resolve('src'),
+      common: resolve('src/common'),
+      components:resolve('src/components')
     }
   },
   module: {
---
+++ src/components/rank/rank.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>排行页面</div>
+</template>
+
+<script>
+export default {}
+</script>
