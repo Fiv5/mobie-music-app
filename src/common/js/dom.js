db8afeb9a841a6fe8cd2adbb85eb439b3b4cde05
Fiv5 <15168273390@163.com>
2018-04-02 17:29:48 +0800

    1.添加歌手列表及歌曲排序, 实现左右联动;2. 实现标题栏固定滑动效果

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -9,3 +9,12 @@ export const hasClass = (el, className) => {
   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
   return reg.test(el.className)
 }
+
+export const getData = (el, name, value) => {
+  const prefix = 'data-'
+  if (value) {
+    return el.setAttribute(prefix + name, value)
+  } else {
+    return el.getAttribute(prefix + name)
+  }
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="singer"></div>
+  <div class="singer">
+    <list-view :data="singers"></list-view>
+  </div>
 </template>
 
 <script>
 import { getSingerList } from 'src/api/singer'
 import { ERR_OK } from 'src/api/config'
+import Singer from 'common/js/singer'
+import ListView from 'src/base/listview/listview'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
 export default {
   data() {
     return { singers: [] }
@@ -15,21 +22,69 @@ export default {
   methods: {
     _getSingerList() {
       getSingerList().then(res => {
-        console.log(res)
         if (res.code === ERR_OK) {
-          this.singers = res.data.list
-          console.log(this.singers)
+          this.singers = this._normalizeSinger(res.data.list)
         }
       })
     },
+    _normalizeSinger(list) {
+      const map = {
+        hot: {
+          title: HOT_NAME,
+          items: [],
+        },
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(
+            new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name,
+            })
+          )
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: [],
+          }
+        }
+
+        map[key].items.push(
+          new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name,
+          })
+        )
+      })
+
+      // 顺序遍历map
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+
+      ret.sort((a, b) => a.title.charCodeAt(0) - b.title.charCodeAt(0))
+      return hot.concat(ret)
+    },
+  },
+  components: {
+    ListView,
   },
 }
 </script>
 
 <style lang="stylus" scoped>
   .singer
-    position:fixed
-    text-outline 88px
-    bottom:0
-    width:100%
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
